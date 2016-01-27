58c7d4550b54bf64b02cfbb08927a34574441b99
forero <j.e.forero.romero@gmail.com>
2016-01-27 14:32:29 -0500

    oscilador armonico con leapfrog

--- weeks/02/code/OsciladorArmonico.c
+++ weeks/02/code/OsciladorArmonico.c
@@ -2,13 +2,16 @@
 #include <math.h>
 
 
-void RK4_step(double * x_step, double * v_step, double delta_t, double t, double x, double v);
+void RK4_step(double * x_step, double * v_step, double delta_t, double t, 
+	      double x, double v);
+void leapfrog_step(double * x_step, double  * v_step, double delta_t, 
+		   double t, double x, double v);
 double deriv_x(double t, double x, double v);
 double deriv_v(double t, double x, double v);
 
 int main(){
-  double x;
-  double v;
+  double x_RK4, x_LF;
+  double v_RK4, v_LF;
   double t;
 
   double T=1E5;
@@ -18,15 +21,25 @@ int main(){
   double v_step = 0.0;
   int i;
   t=0.0;
-  x=1.0;
-  v=0.0;
+  x_RK4=1.0;
+  v_RK4=0.0;
+
+  x_LF=1.0;
+  v_LF=0.0;
   
   for(i=0;i<n_step;i++){    
-    printf("%f %.15e %.15e \n", t, x, v);
-    RK4_step(&x_step, &v_step, delta_t, t, x, v);
-    x += x_step;
-    v += v_step;
+    printf("%f %.15e %.15e %.15e %.15e \n", t, x_RK4, v_RK4, x_LF, v_LF);
+
+    RK4_step(&x_step, &v_step, delta_t, t, x_RK4, v_RK4);
+    x_RK4 += x_step;
+    v_RK4 += v_step;
+
+    leapfrog_step(&x_step, &v_step, delta_t, t, x_LF, v_LF);
+    x_LF += x_step;
+    v_LF += v_step;
+
     t += delta_t;
+
   }
 
   return 0;
@@ -38,6 +51,22 @@ double deriv_x(double t, double x, double v){
 
 double deriv_v(double t, double x, double v){
   return -x;
+}
+
+void leapfrog_step(double * x_step, double  * v_step, double delta_t, double t, double x, double v){
+  double step_x;
+  double step_v;
+  /*kick*/
+  step_v = deriv_v(t,x,v) * delta_t * 0.5;
+
+  /*drift*/
+  step_x = (v + step_v) * delta_t;
+
+  /*kick*/
+  step_v = step_v + deriv_v(t, x + step_x, v) * delta_t * 0.5;
+
+  *x_step = step_x;
+  *v_step = step_v;
 }
 
 void RK4_step(double * x_step, double  * v_step, double delta_t, double t, double x, double v){
