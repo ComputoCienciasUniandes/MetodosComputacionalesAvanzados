094c24d59fb27dc8f9d2e6d5656fd25c0ad84d0a
Metodos computacionales avanzados je.forero <j.e.forero.romero@gmail.com>
2017-02-06 16:58:58 -0500

    block

--- weeks/03/code/mpi_3/sendrecv_b.c
+++ weeks/03/code/mpi_3/sendrecv_b.c
@@ -8,27 +8,25 @@
 int main(int argc, char **argv){
   int rank, source, destination, in_number, out_number;
 
-
-  printf("asdfasdf");
   MPI_Init(NULL, NULL);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
   out_number = 42;
-  printf("%d\n", rank);
+
   if(rank==0){
     destination = 1;
     source = 1;
+    MPI_Recv(&in_number, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Send(&out_number, 1, MPI_INT, destination, 0, MPI_COMM_WORLD);
-    //    MPI_Recv(&in_number, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-    //    printf("process %d sent %d received by %d\n", source, number, rank);
+   
+    printf("proc %d sent %d (received by proc %d)\n", source, in_number, rank);
   }else if (rank==1){
     destination = 0;
-    source = 0;
-
-    //    MPI_Send(&out_number, 1, MPI_INT, destination, 0, MPI_COMM_WORLD);
+    source = 0;    
     MPI_Recv(&in_number, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Send(&out_number, 1, MPI_INT, destination, 0, MPI_COMM_WORLD);
 
-    printf("process %d sent %d received by %d\n", source, number, rank);
+    printf("proc %d sent %d (received by proc %d)\n", source, in_number, rank);
   }
   MPI_Finalize();
   return 0;
