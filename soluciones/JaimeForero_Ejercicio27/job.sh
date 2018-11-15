0b751a40f60512dc2bc56995bbb3da3e7150bab5
Curso fisi4028  je.forero 2018-09-26 <fisi4028@clustergate.local>
2018-11-15 17:12:22 -0500

    script

---
+++ soluciones/JaimeForero_Ejercicio27/job.sh
@@ -0,0 +1,11 @@
+#PBS -l nodes=1:ppn=1,mem=1gb,walltime=00:10:00
+#PBS -M je.forero@uniandes.edu.co
+#PBS -m abe
+#PBS -N ejercicio25
+
+
+module load anaconda/python3
+cd $PBS_O_WORKDIR # este es el directorio desde donde se ejecuto qsub
+rm -f *.dat
+mpicc sample.c -o sample.x
+mpirun -np 10 ./a.out 10000 0.0 1.0
